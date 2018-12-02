125f47ebc36ff7581502c51d07ba4b7602ee3aec
Timothy Rogers <choiseul11@gmail.com>
2018-12-02 13:27:19 -0600

    Solved 2018 Day1 Challenge1

---
+++ 2018/DAY1/chronal-calibration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""Solution to Advent of Code 2018 Day 1, Challenge 1: Chronal Calibration"""
+__author__ = "Timothy J. Rogers"
+
+def main():
+    freq = 0
+    with open('chronal-calibration-input.txt', 'r') as f:
+        freq_data = f.readlines()
+    for fr in freq_data:
+        freq = freq + int(fr)
+    print(freq)
+
+if __name__ == "__main__":
+    main()
