2aa1fff1929e48ad977e4a5a6d9fae1e205f8dc6
Timothy Rogers <choiseul11@gmail.com>
2018-12-30 16:27:23 -0600

    Intermediate commit -- roaming work on 2018 DAY9 fast part2 solution

---
+++ 2018/DAY9/marble-mania-pt2.go
@@ -0,0 +1,53 @@
+package main
+
+import ("fmt"; "os"; "bufio"; "strings"; "strconv")
+
+type marble struct {
+	score int
+	next *marble
+	prev *marble
+}
+
+func main() {
+	//initialize file access for input
+	inputPath := "marble-mania-input.txt"
+	file, _ := os.Open(inputPath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	data := strings.Fields(scanner.Text())
+
+	numPlayers, _ := strconv.Atoi(data[0])
+	players := make(map[int]int)
+	for i := 1; i <= numPlayers; i++ { players[i] = 0 }
+	
+	numMarbles, _ := strconv.Atoi(data[6])
+	
+	curMarble := &marble{score: 0, next: nil, prev: nil}
+	curMarble.next = curMarble
+	curMarble.prev = curMarble
+	//zeroMarble = curMarble
+
+	for i := 1; i <= numMarbles; i++ {
+		curPlayer := i % numPlayers
+		if i % 23 == 0 {
+			players[curPlayer] += i
+			for i := 0; i < 7; i++ { curMarble = curMarble.prev }
+			players[curPlayer] += curMarble.score
+			curMarble.prev.next = curMarble.next
+			curMarble.next.prev = curMarble.prev
+			curMarble = curMarble.next
+			continue
+		}
+		nextMarble := &marble{score: i, next: curMarble.next, prev: curMarble}
+		curMarble.next.prev = nextMarble
+		curMarble.next = nextMarble
+	}
+
+	maxScore := 0
+	for _, v := range players {
+		if v > maxScore { maxScore = v }
+	}
+	fmt.Println(maxScore)
+}
