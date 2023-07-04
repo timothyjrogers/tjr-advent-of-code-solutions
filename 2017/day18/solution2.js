6bc938c229adcbc6d0f2fe2849efefa0531ba0a2
Tim Rogers <rogers.timothy.john@gmail.com>
2023-07-04 18:54:22 -0500

    2017 day 18 part 1

--- 2017/day18/solution2.js
+++ 2017/day18/solution2.js
@@ -4,12 +4,25 @@ data = fs.readFileSync('input.txt', 'utf-8');
 const steps = data.trim().split("\n");
 
 let registers = {};
+let registers0 = {};
+let buffer0 = [];
+let pc0 = 0;
+let waiting0 = false;
+let registers1 = {};
+let buffer1 = [];
+let pc1 = 0;
+let waiting1 = false;
+
 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].forEach(letter => {
     registers[letter] = 0;
+    registers0[letter] = 0;
+    registers1[letter] = 0;
 })
-let sound = 0;
-let pc = 0;
-while (pc < steps.length) {
+
+while (pc0 < steps.length && pc1 < steps.length) {
+    if (waiting0 && buffer1.length == 0 && waiting1 && buffer0.length == 0) {
+        break;
+    }
     step = steps[pc];
     let parts = step.split(" ");
     if (parts.length == 2) {
@@ -18,12 +31,9 @@ while (pc < steps.length) {
             val = registers[parts[1]];
         }
         if (parts[0] == "snd") {
-            sound = val;
+            //TODO
         } else {
-            if (val != 0) {
-                console.log(`Part one: ${sound}`);
-                break;
-            }
+            //TODO
         }
     } else if (parts.length == 3) {
         if (parts[0] == "jgz") {
