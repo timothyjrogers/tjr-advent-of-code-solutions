e1170ba5607e93ff09f7894bfc4ed08c09e90ecb
Timothy J. Rogers <rogers.timothy.john@gmail.com>
2023-12-01 20:23:49 -0600

    2023 day 1 part 2 finished

--- 2023/day1/solution.py
+++ 2023/day1/solution.py
@@ -2,14 +2,13 @@ with open('input.txt', 'r') as f:
     data = f.read().splitlines()
 
 sum1 = 0
-data_parsed = []
 sum2 = 0
-# word: (numeral, length)
-digit_words = {'one': ('1',3), 'two': ('2',3), 'three': ('3',5), 'four': ('4',4), 'five': ('5',4), 'six': ('6',3), 'seven': ('7',5), 'eight': ('8',5), 'nine': ('9',4)}
+# word: (modified word, length); inject numeral into word to avoid edge cases like sevenine
+digit_words = {'one': ('on1e',3), 'two': ('tw2o',3), 'three': ('thre3e',5), 'four': ('fou4r',4), 'five': ('fiv5e',4), 'six': ('si6x',3), 'seven': ('seve7n',5), 'eight': ('eigh8t',5), 'nine': ('nin9e',4)}
 for line in data:
     digits = [x for x in line if x.isdigit()]
-    if len(digits) > 1:
-        sum1  += int(digits[0] + digits[len(digits)-1])
+    if len(digits) > 0:
+        sum1  += int(digits[0] + digits[-1])
     
     parsed_line = line
     for k,v in digit_words.items():
@@ -17,9 +16,9 @@ for line in data:
         while index > -1:
             parsed_line = parsed_line[0:index] + v[0] + parsed_line[index+v[1]:len(parsed_line)]
             index = parsed_line.find(k)
-    print(f'{parsed_line} -> {digits[0]}, {digits[len(digits)-1]}')
     digits = [x for x in parsed_line if x.isdigit()]
-    sum2  += int(digits[0] + digits[len(digits)-1])
+    if len(digits) > 0:
+        sum2  += int(digits[0] + digits[-1])    
 
 print(f'Part one: {sum1}')
 print(f'Part two: {sum2}')
