7827c4b06c56f586de4f8580c9699c7e7c292ed2
Timothy J. Rogers <rogers.timothy.john@gmail.com>
2023-12-05 21:49:58 -0600

    2023 Day 5 WIP

--- 2023/day5/solution_p2.py
+++ 2023/day5/solution_p2.py
@@ -11,27 +11,30 @@ seeds.sort()
 print(seeds)
 
 
-def splitRanges(r1, r2):
-    print(f'Source range: {r1}, Destination range: {r2}')
+def splitRanges(r1, ranges):
     new_ranges = []
-    if r1[0] >= r2[0] and r1[0] + r1[2] <= r2[0] + r2[2]:
-        print(f'Source range contained in destination range')
-        new_ranges.append((r1[0], r2[1], r1[2]))
-    elif r1[0] < r2[0] and r1[0] + r1[2] >= r2[0] and r1[0] + r1[2] <= r2[0] + r2[2]:
-        print(f'Source range overlaps left end of destination range')
-        new_range1 = (r1[0], r1[1], r2[0]-r1[0])
-        new_range2 = (r2[0], r2[1], r1[0]+r1[2]-r2[0])
-        new_ranges.append(new_range1)
-        new_ranges.append(new_range2)
-    elif r1[0] >= r2[0] and r1[0] <= r2[0]+r2[2] and r1[0]+r1[2] > r2[0]+r2[2]:
-        print(f'Source range overlaps right end of destination range')
-        new_range1 = (r1[0], r2[1], r2[0]+r2[2]-r1[0])
-        new_range2 = (r2[0]+r2[2]+1, r1[1], r1[0]+r1[2]-r2[0]+r2[2])
-        new_ranges.append(new_range1)
-        new_ranges.append(new_range2)
-    else:
+    for r2 in ranges:
+        print(f'Source range: {r1}, Destination range: {r2}')
+        if r1[0] >= r2[0] and r1[0] + r1[2] < r2[0] + r2[2]:
+            print(f'Source range contained in destination range')
+            new_ranges.append((r1[0], r2[1], r1[2]))
+        elif r1[0] < r2[0] and r1[0] + r1[2] >= r2[0] and r1[0] + r1[2] < r2[0] + r2[2]:
+            print(f'Source range overlaps left end of destination range')
+            new_range1 = (r1[0], r1[1], r2[0]-r1[0])
+            new_range2 = (r2[0], r2[1], r1[0]+r1[2]-r2[0])
+            new_ranges.append(new_range1)
+            new_ranges.append(new_range2)
+        elif r1[0] >= r2[0] and r1[0] < r2[0]+r2[2] and r1[0]+r1[2] > r2[0]+r2[2]:
+            print(f'Source range overlaps right end of destination range')
+            new_range1 = (r1[0], r2[1], r2[0]+r2[2]-r1[0])
+            new_range2 = (r2[0]+r2[2]+1, r1[1], r1[0]+r1[2]-r2[0]+r2[2])
+            new_ranges.append(new_range1)
+            new_ranges.append(new_range2)
+        else:
+            #new_ranges.append(r1)
+            print(f'Source range does not overlap destination range')
+    if len(new_ranges) == 0:
         new_ranges.append(r1)
-        print(f'Source range does not overlap destination range')
     return new_ranges
 
 i = 1
@@ -58,10 +61,9 @@ while i < len(data):
         print(f'Destination ranges for {cur_map}: {map_ranges}')
         new_seed_ranges = []
         for r1 in seeds:
-            for r2 in map_ranges:
-                new_seed_ranges.extend(splitRanges(r1, r2))
+            new_seed_ranges.extend(splitRanges(r1, map_ranges))
         seeds = new_seed_ranges
         print(f'Updated seed ranges: {seeds}')
 
 seeds.sort()
-#print(seeds[0])
+print(seeds[0])
