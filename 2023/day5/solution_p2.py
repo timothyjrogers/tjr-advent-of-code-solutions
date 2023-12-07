b1c5701bba6584770aaaab10393d114138f1555b
Timothy J. Rogers <rogers.timothy.john@gmail.com>
2023-12-07 14:37:47 -0600

    2023 Day 5 Part 2 complete

--- 2023/day5/solution_p2.py
+++ 2023/day5/solution_p2.py
@@ -1,41 +1,8 @@
 with open('input.txt', 'r') as f:
     data = f.read().splitlines()
 
-seed_ranges = [int(x) for x in data[0].split(': ')[1].split()]
-seeds = []
-s = 0
-while s < len(seed_ranges):
-    seeds.append((seed_ranges[s], seed_ranges[s], seed_ranges[s+1]))
-    s += 2
-seeds.sort()
-print(seeds)
 
-
-def splitRanges(r1, ranges):
-    new_ranges = []
-    for r2 in ranges:
-        print(f'Source range: {r1}, Destination range: {r2}')
-        if r1[0] >= r2[0] and r1[0] + r1[2] < r2[0] + r2[2]:
-            print(f'Source range contained in destination range')
-            new_ranges.append((r1[0], r2[1], r1[2]))
-        elif r1[0] < r2[0] and r1[0] + r1[2] >= r2[0] and r1[0] + r1[2] < r2[0] + r2[2]:
-            print(f'Source range overlaps left end of destination range')
-            new_range1 = (r1[0], r1[1], r2[0]-r1[0])
-            new_range2 = (r2[0], r2[1], r1[0]+r1[2]-r2[0])
-            new_ranges.append(new_range1)
-            new_ranges.append(new_range2)
-        elif r1[0] >= r2[0] and r1[0] < r2[0]+r2[2] and r1[0]+r1[2] > r2[0]+r2[2]:
-            print(f'Source range overlaps right end of destination range')
-            new_range1 = (r1[0], r2[1], r2[0]+r2[2]-r1[0])
-            new_range2 = (r2[0]+r2[2]+1, r1[1], r1[0]+r1[2]-r2[0]+r2[2])
-            new_ranges.append(new_range1)
-            new_ranges.append(new_range2)
-        else:
-            #new_ranges.append(r1)
-            print(f'Source range does not overlap destination range')
-    if len(new_ranges) == 0:
-        new_ranges.append(r1)
-    return new_ranges
+maps = { "seed-to-soil": [], "soil-to-fertilizer": [], "fertilizer-to-water": [], "water-to-light": [], "light-to-temperature": [], "temperature-to-humidity": [], "humidity-to-location": []} 
 
 i = 1
 cur_map = ''
@@ -43,27 +10,46 @@ while i < len(data):
     line = data[i]
     if line == '':
         cur_map = ''
-        i += 1
     elif not line[0].isdigit():
         map_name = line.split(' ')[0]
         cur_map = map_name
-        i += 1
     else:
-        map_ranges = []
-        while i < len(data) and data[i] != '':
-            line = data[i]
-            parts = line.split()
-            dest = int(parts[0])
-            src = int(parts[1])
-            length = int(parts[2])
-            map_ranges.append((src, dest, length))
-            i += 1
-        print(f'Destination ranges for {cur_map}: {map_ranges}')
-        new_seed_ranges = []
-        for r1 in seeds:
-            new_seed_ranges.extend(splitRanges(r1, map_ranges))
-        seeds = new_seed_ranges
-        print(f'Updated seed ranges: {seeds}')
+        parts = line.split()
+        dest = int(parts[0])
+        src = int(parts[1])
+        length = int(parts[2])
+        maps[cur_map].append((src, dest, length)) 
+    i += 1
 
-seeds.sort()
-print(seeds[0])
+def findValInRanges(val, ranges):
+    for r in ranges:
+        if r[0] <= val and val < r[0]+r[2]:
+            return r[1] + (val - r[0])
+    return val
+
+
+def findLocation(maps, seed):
+    min_location = 1000000000
+    soil = findValInRanges(seed, maps['seed-to-soil'])
+    fertilizer = findValInRanges(soil, maps['soil-to-fertilizer'])
+    water = findValInRanges(fertilizer, maps['fertilizer-to-water'])
+    light = findValInRanges(water, maps['water-to-light'])
+    temperature = findValInRanges(light, maps['light-to-temperature'])
+    humidity = findValInRanges(temperature, maps['temperature-to-humidity'])
+    location = findValInRanges(humidity, maps['humidity-to-location'])
+    if location < min_location:
+        min_location = location
+    return min_location
+
+seed_ranges = [int(x) for x in data[0].split(': ')[1].split()]
+n = 0
+answer = 1000000000
+while n < len(seed_ranges):
+    print(n)
+    for i in range(seed_ranges[n+1]):
+        loc = findLocation(maps, seed_ranges[n]+i)
+        if loc < answer:
+            answer = loc
+    n += 2
+
+print(f'Part one: {answer}')
